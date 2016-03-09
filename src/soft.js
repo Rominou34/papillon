b806e895c43a8fb506524646500d2e0c3137bbd9
Romain Arnaud <arnaud.romain34@gmail.com>
2016-03-09 19:36:36 +0100

    Update the shitty soft-popup style

--- src/soft.js
+++ src/soft.js
@@ -8,6 +8,7 @@ link.media = 'all';
 head.appendChild(link);
 
 var popup = document.registerElement('soft-popup');
+var popupmask = document.registerElement('soft-popup-mask');
 
 /*
 * This function is the constructor to display <soft-popup> notifications
@@ -18,19 +19,43 @@ var popup = document.registerElement('soft-popup');
 * "Message" ( the message displayed )]
 */
 var softPopup = function(param) {
-  // Creating the element
+  // Creating the element and the mask
+  var softpopupmask = new popupmask();
   var softpopup = new popup();
+
+  // Getting the corresponding color
+  var color = null;
+  switch(param[0]) {
+    case "success":
+      color = "green"; break;
+    case "info":
+      color = "blue"; break;
+    case "warning":
+      color = "yellow"; break;
+    case "alert":
+      color = "red"; break;
+  }
+
   // Adding the text inside the element
-  softpopup.innerHTML = "<span>" + param[1] + "</span>"+
-  "<div><p>" + param[2] + "</p></div>";
+  if(color != null) {
+    softpopup.innerHTML = "<span>" + param[1] + "</span>"+
+    "<div><p>" + param[2] + "</p></div><button class=\""+color+"\">Hide</button>";
+  } else {
+    softpopup.innerHTML = "<span>" + param[1] + "</span>"+
+    "<div><p>" + param[2] + "</p></div><button>Hide</button>";
+  }
+
   try {
     softpopup.setAttribute(param[0], "");
   } catch(err) {
     console.log(err.message);
   }
   // To hide the element on click
-  softpopup.addEventListener("click", function() {
-    this.remove();
+  var button = softpopup.querySelector("button");
+  button.addEventListener("click", function() {
+    this.parentElement.parentElement.remove();
+    this.parentElement.remove();
   });
-  document.body.appendChild(softpopup);
+  document.body.appendChild(softpopupmask);
+  softpopupmask.appendChild(softpopup);
 }
