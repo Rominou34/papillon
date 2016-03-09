4f19a0491d94ae18975a50038cbd310e763e1b24
Romain Arnaud <arnaud.romain34@gmail.com>
2016-03-09 09:53:58 +0100

    Add soft-popup constructor in js

--- src/soft.js
+++ src/soft.js
@@ -1,5 +1,18 @@
-window.onload = function() {
-  document.querySelector("soft-popup").addEventListener("click", function() {
+var popup = document.registerElement('soft-popup');
+
+var softPopup = function(param) {
+  // Creating the element
+  var softpopup = new popup();
+  // Adding the text inside the element
+  softpopup.innerHTML = "<span>" + param[1] + "</span>" + param[2];
+  try {
+    softpopup.setAttribute(param[0], "");
+  } catch(err) {
+    console.log(err.message);
+  }
+  // To hide the element on click
+  softpopup.addEventListener("click", function() {
     this.remove();
   });
+  document.body.appendChild(softpopup);
 }
