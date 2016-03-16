3ccf922f82bf43b5e7522404320f6d0f013adc3c
Romain Arnaud <arnaud.romain34@gmail.com>
2016-03-16 16:29:56 +0100

    Add tooltips

--- src/soft.js
+++ src/soft.js
@@ -3,6 +3,11 @@
 * Romain Arnaud - 2016
 */
 
+window.onload = function() {
+  addToolTipListeners();
+}
+
+
 /*************** SOFT POPUP ***************/
 
 /*
@@ -58,6 +63,7 @@ var softPopup = function(type, header, msg) {
   softpopupmask.appendChild(softpopup);
 }
 
+
 /*************** SOFT PROGRESS BARS ***************/
 
 /*
@@ -69,11 +75,12 @@ var softPopup = function(type, header, msg) {
 * type = boolean ( if 0, we display "currentValue/maxValue", if 1, we display
 * "x %" )
 */
-var softProgressBar = function(minV, maxV, elem) {
+var softProgressBar = function(minV, maxV, elem, height) {
   this.minV = minV;
   this.maxV = maxV;
   var softprogress = document.createElement("div");
   softprogress.className = "soft-progress";
+  softprogress.style.height = height + "px";
 
   softprogress.setAttribute("data-minv",this.minV);
   softprogress.setAttribute("data-maxv",this.maxV);
@@ -85,5 +92,45 @@ var softProgressBar = function(minV, maxV, elem) {
 
 softProgressBar.prototype.setProgress = function(prog) {
   var percent = Math.round(((prog-this.minV) / (this.maxV-this.minV))*100);
+  if(percent > 100) { percent = 100; }
   this.element.querySelector("div").style.width = percent + "%";
+}
+
+
+/*************** SOFT TOOLTIPS ***************/
+
+
+/*
+* This function adds and event listener to all tooltips so they appear on
+* hover
+*/
+
+var addToolTipListeners = function() {
+  var toolTips = document.querySelectorAll(".soft-tooltip");
+  toolTipItems = [].slice.call(toolTips);
+  toolTipItems.forEach(function (item) {
+    //Event listener for hover
+    item.addEventListener("mouseover", function() {
+      var tips = item.querySelectorAll(".tooltip");
+      tipItems = [].slice.call(tips);
+      tipItems.forEach(function (tip) {
+        tip.style.display = "inline-block";
+        var h = "-" + (tip.offsetHeight+30) + "px";
+        var w = "-" + (tip.offsetWidth+30) + "px";
+        //We offset the element
+        if(tip.classList.contains('top')) { tip.style.top = h; }
+        if(tip.classList.contains('right')) { tip.style.right = w; }
+        if(tip.classList.contains('bottom')) { tip.style.bottom = h; }
+        if(tip.classList.contains('left')) { tip.style.left = w; }
+      });
+    });
+    //Event listener for mouse out
+    item.addEventListener("mouseout", function() {
+      var tips = item.querySelectorAll(".tooltip");
+      tipItems = [].slice.call(tips);
+      tipItems.forEach(function (tip) {
+        tip.style.display = "none";
+      });
+    });
+  });
 }
